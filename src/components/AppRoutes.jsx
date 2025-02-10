92e5d13ad9766ecfd64b59b8010ab32c561e3e3a
Marcelo Austria <marcelo.austria@dpc.ph>
2025-02-10 16:59:29 +0800

    Implemented Edit Post

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom"
 import PostList from "../features/post/PostList"
 import PostDetail from "../features/post/PostDetail"
 import NewPost from "../features/post/NewPost"
+import EditPost from "../features/post/EditPost"
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={ <PostList /> } />
             <Route path="/posts/:id" element={ <PostDetail /> } />
+            <Route path="/posts/:id/edit" element={ <EditPost /> } />
             <Route path="/posts/new" element={ <NewPost /> } />
         </Routes>
     )
